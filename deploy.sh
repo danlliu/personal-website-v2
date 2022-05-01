8a5e55e64ac390f672ea23b12daabf1d811361b9
Daniel Liu <66093915+danlliu@users.noreply.github.com>
2022-05-01 19:58:39 -0400

    Move push logic from deploy.sh to deploy.yml

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,3 @@ cp src/*.html build/
 cp src/js/* build/js
 
 # End build process
-
-cd build
-
-git init
-git add -A
-git branch -m main
-git commit -m "Deploy to GitHub Pages"
-
-git push -f git@github.com:danlliu/personal-web.git main:deploy
-
-cd -
