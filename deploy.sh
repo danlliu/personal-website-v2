f512fb92aad7ed82a56821be3a0beb1b496da0b2
Daniel Liu <66093915+danlliu@users.noreply.github.com>
2022-05-01 19:32:50 -0400

    Initial commit, simple files for workflow testing

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -Eeuo pipefail
+set -x
+
+# Begin build process
+
+rm -rf build/
+mkdir -p build/
+mkdir -p build/js
+mkdir -p build/css
+
+cp src/*.html build/
+cp src/js/* build/js
+
+# End build process
+
+cd build
+
+git init
+git add -A
+git commit -m "Deploy to GitHub Pages"
+
+git push -f git@github.com:danlliu/personal-web.git main:deploy
+
+cd -
