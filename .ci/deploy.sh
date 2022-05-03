5dc8bcbcb9662058487c28cdec8b2884459d425c
Daniel Liu <66093915+danlliu@users.noreply.github.com>
2022-05-03 01:30:20 -0400

    Directory empty after less compilation?

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -11,9 +11,11 @@ cp -R src/ build/
 [ -d "build/css" ] && mkdir -p build/css || true
 
 ls
+ls build
 
 [ -d "src/less" ] && for f in src/less/*.less; do lessc $f > build/css/$(basename $f .less).css; done || true
 
+ls
 ls build
 
 python build/render_templates.py
