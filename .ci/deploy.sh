2451ebb672a91a350dd6962f26ee7b50bd7b9325
Daniel Liu <66093915+danlliu@users.noreply.github.com>
2022-05-03 01:28:38 -0400

    Add ls, working to debug workflow

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -9,7 +9,12 @@ mkdir build/
 
 cp -R src/ build/
 [ -d "build/css" ] && mkdir -p build/css || true
+
+ls
+
 [ -d "src/less" ] && for f in src/less/*.less; do lessc $f > build/css/$(basename $f .less).css; done || true
+
+ls build
 
 python build/render_templates.py
 
