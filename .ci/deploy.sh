482f1a626424323b83089f32e2db43e526a86cd9
Daniel Liu <66093915+danlliu@users.noreply.github.com>
2022-08-07 22:52:50 -0400

    Add internship, fix deploy.sh

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -9,7 +9,7 @@ cp -r src build
 [ -d "build/css" ] || mkdir -p build/css
 [ -d "src/less" ] && for f in src/less/*.less; do lessc $f > build/css/$(basename $f .less).css; done || true
 
-python build/fetch_icon_colors.py
-python build/render_templates.py
+python3 build/fetch_icon_colors.py
+python3 build/render_templates.py
 
 # End build process
